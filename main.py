bee6e0ae872cf6ee52bc6c3ba1f0da485a203108
Fevronija Rybkina <anton.tsizov@tkvg.ee>
2026-03-28 21:36:28 +0200

    PYDANTIC

--- main.py
+++ main.py
@@ -2,37 +2,38 @@ from colorama import *
 import psycopg2 as ps
 import os
 from typing import Annotated
-#from pydantic import BaseModel
-#from pydantic import Field
-#class Platok_Schema(BaseModel):
-    #id: int
-    #Название_Платка: str = Field(min_length=5, max_length=50)
-    #Автор_Платка: str = Field(min_length=5, max_length=50)
-    #Колорит_1: str= Field(min_length=3, max_length=50)
-    #Колорит_2: str= Field(min_length=3, max_length=50)
-    #Колорит_3: str= Field(min_length=3, max_length=50)
-    #Колорит_4: str= Field(min_length=3, max_length=50)
-    #Колорит_5: str= Field(min_length=3, max_length=50)
-    #Узор_Темени: str= Field(min_length=3, max_length=50)
-    #Узор_Сердцевины: str= Field(min_length=3, max_length=50)
-    #Узор_Сторон: str= Field(min_length=3, max_length=50)
-    #Узор_Углов: str= Field(min_length=3, max_length=50)
-    #Узор_Края: str= Field(min_length=3, max_length=50)
-    #Цветы_Орнамент: str= Field(min_length=3, max_length=50)
-    #Изображённый_Цветок_1: str= Field(min_length=3, max_length=50)
-    #Изображённый_Цветок_2: str= Field(min_length=3, max_length=50)
-    #зображённый_Цветок_3: str= Field(min_length=3, max_length=50)
-    #Изображённый_Цветок_4: str= Field(min_length=3, max_length=50)
-    #Изображённый_Цветок_5: str= Field(min_length=3, max_length=50)
-    #Размер_Платка: str= Field(min_length=3, max_length=50)
-    #Материал_Платка: str= Field(min_length=3, max_length=50)
-    #Материал_Бахромы: str= Field(min_length=3, max_length=50)
+from pydantic import BaseModel, ValidationError
+from pydantic import Field
+class Platok_Schema(BaseModel):
+    id: int
+    Название_Платка: str = Field(min_length=5, max_length=50)
+    Автор_Платка: str = Field(min_length=5, max_length=50)
+    Колорит_1: str= Field(min_length=3, max_length=50)
+    Колорит_2: str= Field(min_length=3, max_length=50)
+    Колорит_3: str= Field(min_length=3, max_length=50)
+    Колорит_4: str= Field(min_length=3, max_length=50)
+    Колорит_5: str= Field(min_length=3, max_length=50)
+    Узор_Темени: str= Field(min_length=3, max_length=50)
+    Узор_Сердцевины: str= Field(min_length=3, max_length=50)
+    Узор_Сторон: str= Field(min_length=3, max_length=50)
+    Узор_Углов: str= Field(min_length=3, max_length=50)
+    Узор_Края: str= Field(min_length=3, max_length=50)
+    Цветы_Орнамент: str= Field(min_length=3, max_length=50)
+    Изображённый_Цветок_1: str= Field(min_length=3, max_length=50)
+    Изображённый_Цветок_2: str= Field(min_length=3, max_length=50)
+    Изображённый_Цветок_3: str= Field(min_length=3, max_length=50)
+    Изображённый_Цветок_4: str= Field(min_length=3, max_length=50)
+    Изображённый_Цветок_5: str= Field(min_length=3, max_length=50)
+    Размер_Платка: str= Field(min_length=3, max_length=50)
+    Материал_Платка: str= Field(min_length=3, max_length=50)
+    Материал_Бахромы: str= Field(min_length=3, max_length=50)
 from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
 from streamlit import streamlit as stml, chat_message
 #cmd='streamlit run main.py --server.port 1000'
 stml.title('ДОБАВИТЬ ПЛАТОК')
 with stml.form(key='ДОБАВИТЬ ПЛАТОК'):
+    id=stml.number_input(max_value=200, min_value=1)
     Название_Платка = stml.text_input(label='Название_Платка')
     Автор_Платка = stml.text_input(label='Автор_Платка')
     Колорит_1 = stml.text_input(label='Колорит_1')
@@ -56,7 +57,34 @@ with stml.form(key='ДОБАВИТЬ ПЛАТОК'):
     Материал_Бахромы = stml.text_input(label='Материал_Бахромы')
     submit_button=stml.form_submit_button(label='Отправить')
 if submit_button:
-    print(Материал_Бахромы)
+    platok_vvod={}
+    platok_vvod["id"] = id
+    platok_vvod["Название_Платка"]=Название_Платка
+    platok_vvod["Автор_Платка"]=Автор_Платка
+    platok_vvod["Колорит_1"] = Колорит_1
+    platok_vvod["Колорит_2"] = Колорит_2
+    platok_vvod["Колорит_3"] = Колорит_3
+    platok_vvod["Колорит_4"] = Колорит_4
+    platok_vvod["Колорит_5"] = Колорит_5
+    platok_vvod["Узор_Темени"] = Узор_Темени
+    platok_vvod["Узор_Сердцевины"] = Узор_Сердцевины
+    platok_vvod["Узор_Сторон"] = Узор_Сторон
+    platok_vvod["Узор_Сторон"] = Узор_Углов
+    platok_vvod["Узор_Края"] = Узор_Края
+    platok_vvod["Цветы_Орнамент"] = Цветы_Орнамент
+    platok_vvod["Изображённый_Цветок_1"] = Изображённый_Цветок_1
+    platok_vvod["Изображённый_Цветок_2"] = Изображённый_Цветок_2
+    platok_vvod["Изображённый_Цветок_3"] = Изображённый_Цветок_3
+    platok_vvod["Изображённый_Цветок_4"] = Изображённый_Цветок_4
+    platok_vvod["Изображённый_Цветок_5"] = Изображённый_Цветок_5
+    platok_vvod["Размер_Платка"] = Размер_Платка
+    platok_vvod["Материал_Платка"] = Материал_Платка
+    platok_vvod["Материал_Бахромы"] = Материал_Бахромы
+    try:
+        platok_kontrol=Platok_Schema(**platok_vvod)
+        stml.toast(platok_kontrol)
+    except ValidationError as e:
+        stml.warning("FAIL")
 if __name__ == '__main__':
     init(autoreset=True)
     from streamlit.runtime.scriptrunner import get_script_run_ctx
